e55f0e7b1b5eb2d6828cb50544bf05feb73745ce
user <user@users-MacBook-Pro.local>
2021-05-14 23:18:46 +0700

    lesson3

--- Lession1/src/App.java
+++ Lession1/src/App.java
@@ -1,6 +1,8 @@
 import lesson1.Caculator;
-import lesson2.CaculatorDaysInMonth;
-import lesson2.FomularBMI;
+import lesson2.*;
+import lesson2.*;
+import lesson3.*;
+import java.util.Scanner;
 
 public class App {
     int a = 10;
@@ -8,7 +10,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         int b = 10;
-        App m = new App();
+        /*App m = new App();
         m.Show();
         Caculator c = new Caculator();
         c.display();
@@ -21,7 +23,29 @@ public class App {
         CaculatorDaysInMonth day = new CaculatorDaysInMonth();
         day.daysInMonth(2);
         day.giaiPTBac2(0, 1, 8.9);
-        day.findTriangle(0, 2, 5);
+        day.findTriangle(0, 2, 5);*/
+        Scanner scanner = new Scanner(System.in);
+        PrimesNumber pri = new PrimesNumber();
+        int dem = 0; // đếm số số nguyên tố
+        int i = 2;   // tìm số nguyên tố bắt dầu từ số 2
+        System.out.print("Nhập n = ");
+        int n = scanner.nextInt();
+        System.out.printf("%d số nguyên tố đầu tiên là: \n", n);
+        while (dem < n) {
+            if (pri.isPrimeNumber(i)) {
+                System.out.print(i + " ");
+                dem++;
+            }
+            i++;
+        }
+        if (n >= 2) {
+            System.out.print(2);
+        }
+        for (int j = 3; j < n; j+=2) {
+            if (pri.isPrimeNumberlessOnehundred(j)) {
+                System.out.print(" " + i);
+            }
+        }
 
 
         //System.out.println(c.divide());
