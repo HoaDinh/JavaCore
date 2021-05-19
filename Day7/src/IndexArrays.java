8b0fc95e3abc9a95ddf1be2bca72a5d25556cebf
Hoa Dinh <hoa.dinh@onemount.com>
2021-05-19 17:39:50 +0700

    day7

---
+++ Day7/src/IndexArrays.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class IndexArrays {
+    //Tìm số lớn nhất, nhỏ nhất trong mảng và index
+    public static void findMaxMinNumber()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập số phần tử mảng\n");
+       int size = sc.nextInt();
+        int array[] = new int[size];
+        int maxValue =0;
+        int minValue =0;
+        int indexMax=0;
+        int indexMin=0;
+        for(int i=0; i<size;i++)
+        {
+            System.out.printf("\nNhập phần tử thứ %d ",i);
+            if (i==1)
+            {
+             maxValue =array[0];
+             minValue =array[0];
+            }
+            array[i]=sc.nextInt();
+            if(maxValue<array[i])
+            {
+                maxValue=array[i];
+                indexMax=i;
+
+            }
+            if(minValue>array[i])
+            {
+                minValue=array[i];
+                indexMin=i;
+            }
+
+        }
+        System.out.printf("Giá trị lớn nhất trong mảng là %d với index là %d",maxValue,indexMax);
+        System.out.printf("\nGiá trị nhỏ nhất trong mảng là %d với index là %d",minValue,indexMin);
+
+    }
+    public static void findMaxSecond()
+    {
+        int array [] = {0,8,6,9,8,4,10};
+        int max = array[0];
+        int second=0;
+        int index=0;
+
+        for (int i=0;i<array.length; i++)
+        {
+            if(array[i]>=max)
+            {
+                second=max;
+                max=array[i];
+            }
+            else if(array[i]>second)
+            {
+                second=array[i];
+                index=i;
+            }
+        }
+        System.out.printf("Số lớn thứ hai trong chuỗi là %d với index bằng %d",second,index);
+    }
+
+}
---
+++ Day7/src/Main.java
@@ -0,0 +1,141 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main (String [] args) {
+        // floadArrays();
+        // printArray();
+        // findEvenNumber();
+        int[] arr = {1, 9, 0, 7, 10, 6, 8};
+        sortASC(arr);
+        sortDesc(arr);
+        /*for (int i = 0; i < 7; i++){
+            System.out.printf(" %d\t", arr[i]);
+            }*/
+
+      // IndexArrays.findMaxMinNumber();
+       IndexArrays.findMaxSecond();
+
+
+    }
+    static void floadArrays()
+    {
+
+        float fltarray [] = {0.5f,2.9f,4f,5f};
+        for (int i=0; i<fltarray.length;i++)
+        {
+            System.out.format("%s",fltarray[i]);
+
+
+
+        }
+
+
+    }
+    static void printArray()
+    {
+        int count=0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số phần tử mảng \n");
+        int length = scanner.nextInt();
+        int array[] = new int[length];
+        for (int i=0; i<array.length;i++)
+        {
+            System.out.println("\nnhập phần tử ở vị trí " +i);
+            array[i]=scanner.nextInt();
+
+            if(i==2)
+            {
+                System.out.println("phần tử thứ 2 là"+array[i]);
+                count+=array[i];
+            }
+
+
+
+        }
+        System.out.printf("\nTổng các phần tử trong mảng là " +count);
+
+
+
+    }
+    static void findEvenNumber()
+    {
+        int count=0;
+        int array[]={1,6,3,9,5,0,4};
+        for (int i=0; i<array.length;i++)
+        {
+            if (array[i]%2==0)
+            {
+                System.out.printf("\nCác số chẵn là %d",array[i]);
+                count+=array[i];
+
+            }
+
+
+        }
+        System.out.printf("\n Tổng các số chẵn là %d",count);
+
+    }
+    public static void sortASC(int [] arr) {
+        int temp = arr[0];
+        System.out.println("\nDãy số được sắp xếp tăng dần: ");
+        for (int i = 0 ; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+
+                }
+            }
+            System.out.println(arr[i]);
+        }
+    }
+    public static void sortDesc(int [] arr) {
+        int temp = arr[0];
+        System.out.println("\nDãy số được sắp xếp giảm dần: ");
+        for (int i = 0 ; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+
+                }
+            }
+            System.out.println(arr[i]);
+        }
+    }
+    /*public static void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, end);
+        }
+    }
+    private static int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
+    }*/
+    public static void show(int [] xyz) {
+        for (int i = 0; i < xyz.length; i++) {
+            System.out.print(xyz[i] + " ");
+        }
+    }
+}
